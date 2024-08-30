c1dae583d0899fc45298b6ccf17b6db089a76ae7
ravindu629 <ravinduwijerathne629@gmail.com>
2024-08-30 23:51:57 +0530

    configured context api with typescript

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,19 +1,21 @@
+import { useContext } from "react";
+import { TodosContext } from "../store/todos-context";
 import Todo from "../models/todo";
 import Item from "./Item";
 import classes from "./Todos.module.css";
 
 //when you create a new functional component you should add this React.FC type annotation
 //merge our own prop definitions with those base props ex: <{ items: Todo[] }>
-const Todos: React.FC<{ items: Todo[]; onRemoveTodo: (id: string) => void }> = (
-  props
-) => {
+const Todos: React.FC = () => {
+  const todosCtx = useContext(TodosContext);
+
   return (
     <ul className={classes.todos}>
-      {props.items.map((item) => (
+      {todosCtx.items.map((item) => (
         <Item
           key={item.id}
           text={item.text}
-          onRemoveTodo={props.onRemoveTodo.bind(null, item.id)}
+          onRemoveTodo={todosCtx.removeTodo.bind(null, item.id)}
         />
       ))}
     </ul>
